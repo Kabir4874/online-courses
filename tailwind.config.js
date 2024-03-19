e2792e8143ab041c1636386661dd06011dbb35b9
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-19 19:34:49 +0600

    add

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray10: "#1A1A1A",
+        gray15: "#262626",
+        gray20: "#333333",
+        gray30: "#4C4C4C",
+        gray35: "#59595A",
+        gray40: "#656567",
+        orange50: "#FF9500",
+        orange90: "#FFEACC",
+        orange95: "#FFF4E5",
+        orange97: "#FFF9F0",
+        white95: "#F1F1F3",
+        white97: "#F7F7F8",
+        white99: "#FCFCFD",
+        black20: "#00000033",
+        white30: "#FFFFFF4D",
+        white20: "#FFFFFF33",
+      },
+    },
+  },
+  plugins: [],
+}
+
