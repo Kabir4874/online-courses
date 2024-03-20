138917587663c657304211f06601d11a0c384567
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-20 16:26:44 +0600

    add

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -2,7 +2,7 @@ import WhiteButton from "./WhiteButton";
 
 const SectionHeading = ({ heading, subHeading, btn }) => {
   return (
-    <div className=" mt-[3.13rem] lg:mt-24 flex flex-col lg:flex-row lg:items-end self-stretch  gap-5 lg:gap-72 mb-10 lg:mb-16">
+    <div className=" mt-[3.13rem] lg:mt-24 flex flex-col lg:flex-row lg:items-end self-stretch  gap-5 lg:gap-72 mb-10 lg:mb-16 lg:justify-between">
       <div>
         <h2 className=" self-stretch text-gray15 text-3xl lg:text-5xl font-semibold mb-1 lg:mb-4">
           {heading}
